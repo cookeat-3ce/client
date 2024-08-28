cc94f015fe9743580d8089d47d7099dcbf2191a3
yangjaehyuk <gndksakxk10@naver.com>
2024-08-28 20:57:13 +0900

    :sparkles: Feat : 최신 순 슥쿡 조회(10개씩)

--- src/pages/SskcookRecent/index.jsx
+++ src/pages/SskcookRecent/index.jsx
@@ -1,7 +1,91 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import {
+  Container,
+  TextContainer,
+  SkeletonContainer,
+  CardContainer,
+  CardWrapper,
+} from '../Tag/styles';
+import CustomText from '../../components/Text';
+import { COLORS } from '../../constants';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import instance from '../../apis';
+import { StyledSskcookSkeleton } from '../Home/styles';
+import Card from '../../components/Card';
 const SskcookRecent = () => {
-  return <div>index</div>;
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+    queryKey: ['SskcookRecent'],
+    queryFn: ({ pageParam = 1 }) =>
+      instance
+        .get(`/sskcook?sort=latest&page=${pageParam}`)
+        .then((res) => res.data),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const allData = data?.pages.flatMap((page) => page.data) || [];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+      const documentHeight =
+        document.documentElement.scrollHeight || document.body.scrollHeight;
+
+      if (windowHeight + scrollTop >= documentHeight - 1) {
+        if (hasNextPage && !isFetching) {
+          setIsLoading(true);
+          fetchNextPage().finally(() => setIsLoading(false));
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [fetchNextPage, hasNextPage, isFetching]);
+  return (
+    <Container>
+      <TextContainer>
+        <CustomText
+          text={'최신순'}
+          fontFamily={'Happiness-Sans-Bold'}
+          fontSize={'1.3vw'}
+          color={COLORS.BLACK}
+        />
+      </TextContainer>
+      <SkeletonContainer>
+        {allData.map((item) => (
+          <CardContainer key={item.sskcookId}>
+            <CardWrapper>
+              <Card
+                url={item.sskcookUrl}
+                sskcookId={item.sskcookId}
+                color={COLORS.BLACK}
+              />
+            </CardWrapper>
+          </CardContainer>
+        ))}
+        {isLoading && (
+          <>
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+            <StyledSskcookSkeleton />
+          </>
+        )}
+      </SkeletonContainer>
+    </Container>
+  );
 };
 
 export default SskcookRecent;
